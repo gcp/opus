gcp/opus

commit 0482c82fea6dd397a7d2ab50dc6cda4965278fc2

Make genversion.bat work in paths with spaces.

win32/genversion.bat
@@ -2,7 +2,7 @@
 
 setlocal enableextensions enabledelayedexpansion
 
-for /f %%v in ('git describe --tags --match "v*"') do set version=%%v
+for /f %%v in ('git --git-dir="%~dp0..\.git" describe --tags --match "v*"') do set version=%%v
 
 if not "%version%"=="" goto :gotversion
 
@@ -16,7 +16,7 @@ goto :gotversion
 
 :getversion
 
-for /f "delims== tokens=2" %%v in (%~dp0..\version.mk) do set version=%%v
+for /f "delims== tokens=2" %%v in ("%~dp0..\version.mk") do set version=%%v
 
 set version=!version:^"=!
 set version=!version: =!
@@ -26,7 +26,7 @@ set version=!version: =!
 set version_out=#define %2 "%version%"
 set version_mk=%2 = "%version%"
 
-echo %version_out%> %1_temp
+echo %version_out%> "%1_temp"
 
 if %version%==unknown goto :skipgenerate
 
@@ -35,12 +35,12 @@ echo %version_mk%>> "%~dp0..\version.mk"
 
 :skipgenerate
 
-echo n | comp %1_temp %1 > NUL 2> NUL
+echo n | comp "%1_temp" "%1" > NUL 2> NUL
 
 if not errorlevel 1 goto exit
 
-copy /y %1_temp %1
+copy /y "%1_temp" "%1"
 
 :exit
 
-del %1_temp
+del "%1_temp"

commit 77880655f652c54415e4b5f03b5984b951f5267b

Fix version.mk parsing in genversion.bat.
This was broken by the changes in 0482c82f to make it work with spaces.

CMD interprets the quotes to mean the argument to FOR is a string,
not a file name. Instead, we expand the path to a short file name.

Signed-off-by: Gian-Carlo Pascutto <gcp@sjeng.org>

win32/genversion.bat
@@ -16,7 +16,7 @@ goto :gotversion
 
 :getversion
 
-for /f "delims== tokens=2" %%v in ("%~dp0..\version.mk") do set version=%%v
+for /f "delims== tokens=2" %%v in (%~dps0..\version.mk) do set version=%%v
 
 set version=!version:^"=!
 set version=!version: =!